fdadd8539655ef90312b44f3c890b4ee2016bcb3
VelinYordanov <velin.iordanov@gmail.com>
2016-11-28 19:46:46 +0200

    Added service to drone-model.js.

--- CoolBizz-draft/models/drone-model.js
+++ CoolBizz-draft/models/drone-model.js
@@ -1,24 +1,28 @@
 const mongoose = require('mongoose');
 
 const droneSchema = mongoose.Schema({
-    name:{
-        type:String,
-        required:true
+    name: {
+        type: String,
+        required: true
     },
-    description:{
-        type:String,
-        required:true
+    description: {
+        type: String,
+        required: true
     },
-    startRentDate : [{
-        type:Date
+    service: {
+        type: String,
+        required: true
+    },
+    startRentDate: [{
+        type: Date
     }],
-    endRentDate : [{
-        type:Date
+    endRentDate: [{
+        type: Date
     }]
 });
 
 module.exports = {
-    droneSchema:droneSchema,
-    Drone:mongoose.model('Drone',droneSchema)
+    droneSchema: droneSchema,
+    Drone: mongoose.model('Drone', droneSchema)
 }
 
