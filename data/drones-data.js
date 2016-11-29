cf4965b9f04970e8b901fb4a9613d3e999b14355
VelinYordanov <velin.iordanov@gmail.com>
2016-11-29 20:16:39 +0200

    Added drones-data.js

---
+++ data/drones-data.js
@@ -0,0 +1,25 @@
+module.exports = function(Drone) {
+    return {
+        getAllDrones() {
+            return new Promise((resolve, reject) => {
+                Drone.find((err, drones) => {
+                    if (err) {
+                        return reject(err);
+                    }
+                    return resolve(drones);
+                })
+            })
+        },
+        getDrone(id) {
+            return new Promise((resolve,reject) => {
+                Drone.findOne({_id:id},(err,drone) => {
+                    if(err) {
+                        return reject(err);
+                    }
+
+                    return resolve(drone);
+                })
+            })
+        }
+    }
+}
