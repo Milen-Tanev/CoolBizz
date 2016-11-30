f6c147e31a35cf631467dbfdf73eb9b14b78095c
Viliana Zhilkova <vilianazhilkova@abv.bg>
2016-11-30 17:12:58 +0200

    Changed app-config.js and config/app-config.js Fixed _layout.pug

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const config = require('./config');
 let dbFactory = require('./db');
 let dataService = require('./data')(dbFactory.getDb());
 
-const app = require('./config/app-config');
+const app = require('./config/app-config')(dataService);
 require('./routers')(app, dataService);
 app.listen(config.PORT, () => console.log(`The app is working on ${config.PORT}`));
+
+//console.log(app);
