63cc4979b32c6b795466fba958878bd0678791a7
VelinYordanov <velin.iordanov@gmail.com>
2016-12-02 13:50:44 +0200

    Added drone profile on drones filtered by service.

--- routers/drones-router.js
+++ routers/drones-router.js
@@ -5,10 +5,11 @@ module.exports = function(app, data) {
 let controller = require('../controllers/drones-controller')(data);
 
 router
-    .get('/', controller.listDrones)
-    .get('/:id',controller.getDroneDetails)
-    .get('/services',controller.getServices);
+    .get('/drones', controller.listDrones)
+    .get('/drones/:id',controller.getDroneDetails)
+    .get('/drones/services',controller.getServices)
+    .get('/services/drones/:id',controller.getDroneDetails);
 
-app.use('/drones',router);
+app.use('/',router);
 };
 
