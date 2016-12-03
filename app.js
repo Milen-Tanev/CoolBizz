c10bfd33ae85f70937fb3610a0a638708689eadb
mihail petrov <mr.mihail.petrov@gmail.com>
2016-12-03 21:33:23 +0200

    set env vars for heroku

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ let dataService = require('./data/data-loader')(dbFactory.getDb());
 
 const app = require('./config/app-config')(dataService);
 require('./routers')(app, dataService);
+var xsss;
 app.listen(config.PORT, () => console.log(`
     The app is working on http://localhost:${config.PORT}
-    Stable version available at https://dronesinaction.herokuapp.com/\n`));
+    Stable version available at ${herokuapp}`));
 
 // console.log(app);
