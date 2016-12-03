1ad5a11c4ccbf84e7df895f95561d6775c9389c8
VelinYordanov <velin.iordanov@gmail.com>
2016-12-03 18:52:02 +0200

    Fixed a bug with the models.

--- models/history-model.js
+++ models/history-model.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose');
 const droneSchema = require('./drone-model').droneSchema;
 
-const historySchema = mongoose.Schema({
+const historySchema = new mongoose.Schema({
     drone:droneSchema,
-    startDate:new Date,
-    endDate:new Date
+    startDate:{
+        type:Date,
+        required:true
+    },
+    endDate:{
+        type:Date,
+        required:true
+    }
 })
 
 module.exports = {
